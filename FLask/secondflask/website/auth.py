1aed75c1d448ad5857f2f602c572c827d8b4482a
leejihee <jihee6986@naver.com>
2022-07-21 23:03:56 +0900

    feat: sign-up POST 추가

--- FLask/secondflask/website/auth.py
+++ FLask/secondflask/website/auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, redirect
+import email
+from flask import Blueprint, render_template, redirect, request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,6 +14,18 @@ def logout():
     return redirect("views.blog_home")  # 로그아웃하면 views의 blog_home으로 리다이렉트됨
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    email = request.form.get('email')
+    print(email)
+
+    username = request.form.get('username')
+    print(username)
+
+    password1 = request.form.get('password1')
+    print(password1)
+
+    password2 = request.form.get('password2')
+    print(password2)
+
     return render_template("signup.html")
